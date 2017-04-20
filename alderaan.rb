56378d5400ac09e4828ff69853e76df82ed84a38
Bruno Ribeiro <brunz36@gmail.com>
2017-04-20 14:30:31 -0400

    Initial commit

---
+++ alderaan.rb
@@ -0,0 +1,10 @@
+require 'awesome_print'
+
+require_relative 'film'
+require_relative 'character'
+
+class Menu
+  ap "test"
+end
+
+star_wars = Menu.new
---
+++ film.rb
@@ -0,0 +1,8 @@
+require 'httparty'
+require 'json'
+
+class Film
+  response = HTTParty.get("http://swapi.co/api/")
+
+  return 
+end
